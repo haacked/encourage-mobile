0c427a5c4f4d227fe25d13ab84e71f4cdf8784dc
Phil Haack <haacked@gmail.com>
2020-01-07 11:01:34 -0800

    Add images to encouragements

--- src/Encourage.Mobile/Models/Encouragement.cs
+++ src/Encourage.Mobile/Models/Encouragement.cs
@@ -4,5 +4,6 @@ namespace Encourage.Mobile.Models
     public class Encouragement
     {
         public string Message { get; set; } = null!;
+        public string? ImageUrl { get; set; }
     }
 }
