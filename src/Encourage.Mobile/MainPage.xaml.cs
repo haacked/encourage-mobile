2012cff8eab2eb8aa0f4edad9dc7882469eccb71
Phil Haack <haacked@gmail.com>
2020-01-10 16:05:40 -0800

    Convert spaces to tabs
    This convinced me: https://www.reddit.com/r/javascript/comments/c8drjo/nobody_talks_about_the_real_reason_to_use_tabs/

--- src/Encourage.Mobile/MainPage.xaml.cs
+++ src/Encourage.Mobile/MainPage.xaml.cs
@@ -6,34 +6,34 @@ using Xamarin.Forms;
 
 namespace Encourage
 {
-    public partial class MainPage : ContentPage
-    {
-        readonly EncouragementRepository _encouragementRepository;
-        readonly EncouragementDatabase _database;
+	public partial class MainPage : ContentPage
+	{
+		readonly EncouragementRepository _encouragementRepository;
+		readonly EncouragementDatabase _database;
 
-        public MainPage(EncouragementDatabase database)
-        {
-            InitializeComponent();
-            _database = database;
-            _encouragementRepository = new EncouragementRepository(_database);
-        }
+		public MainPage(EncouragementDatabase database)
+		{
+			InitializeComponent();
+			_database = database;
+			_encouragementRepository = new EncouragementRepository(_database);
+		}
 
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
 
-            BindableLayout.SetItemsSource(moodsLayout, _database.GetMoodsAsync().Result);
-        }
+			BindableLayout.SetItemsSource(moodsLayout, _database.GetMoodsAsync().Result);
+		}
 
-        async void OnButtonClicked(object sender, EventArgs args)
-        {
-            var button = sender as Button ?? throw new InvalidOperationException("A null button was somehow clicked.");
-            var mood = button.BindingContext as Mood ?? throw new InvalidOperationException("A button with no mood was somehow clicked.");
-            var encouragement = await _encouragementRepository.GetRandomEncouragement(mood);
-            await Navigation.PushAsync(new EncouragementPage
-            {
-                BindingContext = encouragement
-            });
-        }
-    }
+		async void OnButtonClicked(object sender, EventArgs args)
+		{
+			var button = sender as Button ?? throw new InvalidOperationException("A null button was somehow clicked.");
+			var mood = button.BindingContext as Mood ?? throw new InvalidOperationException("A button with no mood was somehow clicked.");
+			var encouragement = await _encouragementRepository.GetRandomEncouragement(mood);
+			await Navigation.PushAsync(new EncouragementPage
+			{
+				BindingContext = encouragement
+			});
+		}
+	}
 }
--- src/Encourage.Mobile/Models/Encouragement.cs
+++ src/Encourage.Mobile/Models/Encouragement.cs
@@ -2,12 +2,12 @@
 
 namespace Encourage.Mobile.Models
 {
-    public class Encouragement : IDatabaseEntity
-    {
-        [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
-        public int MoodId { get; set; }
-        public string Message { get; set; } = null!;
-        public string? ImageUrl { get; set; }
-    }
+	public class Encouragement : IDatabaseEntity
+	{
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+		public int MoodId { get; set; }
+		public string Message { get; set; } = null!;
+		public string? ImageUrl { get; set; }
+	}
 }
