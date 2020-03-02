aab7dae722583bc80db4055940e40a91624e112a
Phil Haack <haacked@gmail.com>
2020-03-02 15:42:37 -0800

    Handle invalid color in the database
    If the user enters a color that's not valid, we need to set it to a default color.

---
+++ src/Encourage.Mobile/Converters/ColorConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Encourage.Mobile.Converters
+{
+	public class ColorConverter : IValueConverter, IMarkupExtension
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var converter = TypeDescriptor.GetConverter(typeof(System.Drawing.Color));
+			try
+			{
+				return converter.ConvertFromString(value as string);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				return System.Drawing.Color.AliceBlue;
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value;
+		}
+
+		public object ProvideValue(IServiceProvider serviceProvider)
+		{
+			return this;
+		}
+	}
+}
